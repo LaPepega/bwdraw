5d191b38612ecaafefc79fad7158f4dbfdc85a88
LaPepega <tmv03092008@gmail.com>
2024-01-06 12:56:29 +0500

    Fixed example

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@
 //!    for i in 0..height {
 //!        for j in 0..width {
 //!            if i == 0 || i == height - 1 || j == 0 || j == width - 1 {
-//!                square.set(j, i, true)
+//!                square.set(j, i, true);
 //!            }
 //!        }
 //!    }
